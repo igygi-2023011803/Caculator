d26d49bfa1c730bfee8c02fb80f3cea43376e8fe
igygi-2023011803 <igygi@naver.com>
2024-10-29 23:27:48 +0900

    첫시작

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import javax.swing.*;
+
+public class Main extends JFrame {
+    JFrame f = new JFrame();
 }
