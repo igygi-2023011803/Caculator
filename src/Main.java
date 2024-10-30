f8eb164298c94d81dbfeeb586d949a2f10a220ab
igygi-2023011803 <igygi@naver.com>
2024-10-30 22:45:24 +0900

    첫시작 틀 만들기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
-    JFrame f = new JFrame();
+    public Main() {
+        setTitle("계산기");
+        setSize(520,250);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JTextField textField = new JTextField();
+        add(textField, BorderLayout.NORTH);
+
+        JPanel button = new JPanel();
+        button.setLayout(new GridLayout(5, 5,3,3));
+
+        add(button, BorderLayout.CENTER);
+        setVisible(true);
+
+    }
+    public static void main(String [] args){
+        new Main();
+    }
+
+
 }
