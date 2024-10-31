88037c3d48bc06998c6324667f7a9dd480b53e95
igygi-2023011803 <igygi@naver.com>
2024-10-31 16:25:52 +0900

    액션 이벤트 수정 계산기 디자인 설정

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
     private  JTextField textField;
-    private  String operator;
+    private  String operator = "";
     private  double num1, num2;
+
     public Main() {
         setTitle("계산기");
         setSize(400,600);
@@ -13,13 +15,17 @@ public class Main extends JFrame {
         setLayout(new BorderLayout());
 
         JPanel PanelA = new JPanel();
+        PanelA.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
         textField = new JTextField(20);
         textField.setEditable(false);
+        textField.setPreferredSize(new Dimension(400, 60));
+        textField.setFont(new Font("Arial", Font.PLAIN, 20));
+        textField.setBorder(BorderFactory.createEmptyBorder());
         PanelA.add(textField);
         add(PanelA, BorderLayout.NORTH);
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setLayout(new GridLayout(5, 4,10,10));
+        buttonPanel.setLayout(new GridLayout(5, 4));
 
         String[] buttons = {
                 "7", "8", "9", "/",
@@ -31,6 +37,10 @@ public class Main extends JFrame {
 
         for (String text : buttons) {
             JButton button = new JButton(text);
+            button.setFont(new Font("Arial", Font.PLAIN, 26));
+            button.setBackground(Color.BLACK);
+            button.setForeground(Color.WHITE);
+            button.setBorder(BorderFactory.createLineBorder(Color.WHITE));
             button.addActionListener(new ButtonClickListener());
             buttonPanel.add(button);
         }
@@ -40,11 +50,53 @@ public class Main extends JFrame {
 
     }
 
-    private class ButtonClickListner implements ActionListener {
+    private class ButtonClickListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
 
-            if (command.charAt)
+            if (command.charAt(0)>= '0' && command.charAt(0) <= '9') {
+                textField.setText(textField.getText() + command);
+            } else if (command.equals("C")) {
+                textField.setText("");
+                operator = "";
+                num1 = num2 = 0;
+            }
+              else if (command.equals("Del")) {
+                 String currentText = textField.getText();
+                 if (currentText.length() > 0) {
+                     textField.setText(currentText.substring(0, currentText.length() -1));
+                 }
+            }
+              else if (command.equals("=")) {
+                num2 = Double.parseDouble(textField.getText());
+                switch (operator) {
+                    case "+":
+                        textField.setText(String.valueOf(num1 + num2));
+                        break;
+                    case "-":
+                        textField.setText(String.valueOf(num1 - num2));
+                        break;
+                    case "x":
+                        textField.setText(String.valueOf(num1 * num2));
+                        break;
+                    case "/":
+                        if (num2 != 0) {
+                            textField.setText(String.valueOf(num1 / num2));
+                        } else {
+                            textField.setText(String.valueOf(num1 / num2));
+                        }
+                        break;
+                }
+                operator = "";
+            } else {
+                if (!operator.isEmpty()) {
+                    return;
+                }
+                operator = command;
+                num1 = Double.parseDouble(textField.getText());
+                textField.setText("");
+            }
+        }
 
     }
 
